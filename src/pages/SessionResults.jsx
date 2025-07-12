995bcb638c41be88c70da436a3533697c78edc11
fooood14 <ailovabl0@gmail.com>
2025-07-12 02:07:13 +0100

    Update SessionResults.jsx

--- src/pages/SessionResults.jsx
+++ src/pages/SessionResults.jsx
@@ -97,7 +97,8 @@ const questionSourceId = testData.original_test_id || testData.id;
     const { data: questionsData, error: questionsError } = await supabase
   .from('questions')
   .select('*')
-  .eq('test_id', questionSourceId)
+.eq('test_id', testId)
+
   .order('order', { ascending: true });
 
     if (questionsError) {
